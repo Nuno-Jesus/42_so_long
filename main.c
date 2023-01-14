414cd6d7cffa5edb5682aec145c08ea8923719de
ncarvalh <ncarvalh@student.42porto.com>
2023-01-14 19:40:12 +0000

    Code: understood how memmory allocation works.

--- main.c
+++ main.c
@@ -6,20 +6,52 @@
 /*   By: ncarvalh <ncarvalh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/10 13:04:07 by ncarvalh          #+#    #+#             */
-/*   Updated: 2023/01/14 15:44:46 by ncarvalh         ###   ########.fr       */
+/*   Updated: 2023/01/14 19:40:02 by ncarvalh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/so_long.h"
 
-int main(int argc, char **argv)
-{
-	(void)argc;
-	(void)argv;
+typedef struct	s_graphic {
 	void	*mlx;
-	void	*mlx_win;
+	void	*win;
+	void	*img;
+	char	*addr;
+	int		bpp;
+	int		line_length;
+	int		endian;
+}				t_graphic;
 
-	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
-	mlx_loop(mlx);
+void	my_mlx_pixel_put(t_graphic *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bpp / 8));
+	*(unsigned int*)dst = color;
+}
+
+int f(int keycode, t_graphic *data)
+{
+	printf("%d\n", keycode);
+		
+	mlx_destroy_image(data->mlx, data->img);
+	mlx_destroy_window(data->mlx, data->win);
+	mlx_destroy_display(data->mlx);
+	free(data->mlx);
+	exit(EXIT_SUCCESS);
+	return (keycode);
+}
+
+int	main(void)
+{
+	t_graphic	data;
+
+	data.mlx = mlx_init();
+	data.win = mlx_new_window(data.mlx, 512, 512, "Hello world!");
+	data.img = mlx_new_image(data.mlx, 512, 512);
+	data.addr = mlx_get_data_addr(data.img, &data.bpp, &data.line_length, &data.endian);
+	my_mlx_pixel_put(&data, 5, 5, 0xFF0000);
+	mlx_put_image_to_window(data.mlx, data.win, data.img, 0, 0);
+	mlx_key_hook(data.win, f, &data);
+	mlx_loop(data.mlx);
 }
