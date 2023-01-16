4a37afda55e35f39ed6c4e0c88eff006fd54cff6
ncarvalh <ncarvalh@student.42porto.com>
2023-01-16 19:02:38 +0000

    Code: handled filename validity.

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: ncarvalh <ncarvalh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/10 13:04:07 by ncarvalh          #+#    #+#             */
-/*   Updated: 2023/01/16 18:52:04 by ncarvalh         ###   ########.fr       */
+/*   Updated: 2023/01/16 19:02:19 by ncarvalh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,16 +33,31 @@ int handler(int keycode, t_game *game)
 	return (keycode);
 }
 
-int	main(void)
+bool is_valid(char *filename)
+{
+	size_t	len;
+
+	len = ft_strlen(filename);
+	if (len < 4)
+		return (false);
+	return (ft_strnstr(filename + (len - 4), ".ber", 4) != NULL);	
+}
+
+int	main(int argc, char **argv)
 {
 	t_game	g;
-
+	
+	if (argc - 1 != 1)
+		message(NULL, "Invalid number of parameters.\n");
+	if (!is_valid(argv[1]))
+		message(NULL, "Filename must end in \".ber\".\n");
+	
 	g.map = NULL;
 	g.display.mlx = mlx_init();
-	g.display.win = mlx_new_window(g.display.mlx, 512, 512, "Hello world!");
+	g.display.win = mlx_new_window(g.display.mlx, 512, 512, "so_long");
 	g.display.img = mlx_new_image(g.display.mlx, 512, 512);
 	g.display.addr = mlx_get_data_addr(g.display.img, &(g.display.bpp), &(g.display.line_length), &(g.display.endian));
-	my_mlx_pixel_put(&(g.display), 5, 5, 0xFF0000);
+	my_mlx_pixel_put(&g.display, 5, 5, 0xFF0000);
 	mlx_put_image_to_window(g.display.mlx, g.display.win, g.display.img, 0, 0);
 	mlx_key_hook(g.display.win, handler, &g);
 	mlx_loop(g.display.mlx);
