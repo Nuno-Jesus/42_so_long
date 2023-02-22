9152d9bd4ea07c3721447bae2c947f97079db4e2
Nuno Jesus <ncarvalh@student.42porto.com>
2023-02-22 13:32:19 +0000

    Files: replaced potion xpm.

--- includes/macros_bonus.h
+++ includes/macros_bonus.h
@@ -6,7 +6,7 @@
 /*   By: ncarvalh <ncarvalh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/18 02:39:20 by marvin            #+#    #+#             */
-/*   Updated: 2023/02/22 11:47:26 by ncarvalh         ###   ########.fr       */
+/*   Updated: 2023/02/22 13:31:24 by ncarvalh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,7 +38,7 @@
 
 //! Sprite refreshing frequency
 # define CALLS				250
-# define CALLS_PER_FRAME	48
+# define CALLS_PER_FRAME	8
 
 //! Binary map needed macros
 # define DIFF				0
