2ce3ddfba930d24c508063fa710e350ee6d0a844
Nuno Jesus <ncarvalh@student.42porto.com>
2023-02-22 21:59:31 +0000

    Refactoring: applied a small refactoring in animation functions.

--- srcs_bonus/animate.c
+++ srcs_bonus/animate.c
@@ -6,7 +6,7 @@
 /*   By: ncarvalh <ncarvalh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/22 10:48:55 by ncarvalh          #+#    #+#             */
-/*   Updated: 2023/02/22 21:15:32 by ncarvalh         ###   ########.fr       */
+/*   Updated: 2023/02/22 21:58:52 by ncarvalh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,10 @@ void	animate_player(t_game *g)
 	static int	calls = 0;
 	static int	freq = 0;
 
-	++calls;
-	if (calls % CALLS != 0)
-		return ;		
-	++freq;
-	if (freq % CALLS_PER_FRAME != 0)
+	if (++calls % CALLS != 0)
 		return ;
-	g->player->current_frame = ++(g->player->current_frame) % NUM_PLAYER_FRAMES;
+	if (++freq % CALLS_PER_FRAME == 0)
+		g->player->current_frame = ++(g->player->current_frame) % NUM_PLAYER_FRAMES;
 	render_sprite(g, &g->sp, g->curr, S1);
 	render_sprite(g, &g->pframes[g->player_dir], \
 		g->curr, g->player->current_frame);
@@ -36,16 +33,15 @@ void	animate_coins(t_game *g)
 	unsigned int	i;
 
 	i = -1;
-	++calls;
-	if (calls % CALLS != 0)
+	if (++calls % CALLS != 0)
 		return ;
-	++freq;
-	if (freq % CALLS_PER_FRAME != 0)
-		return ;	
+	if (++freq % CALLS_PER_FRAME != 0)
+		return ;
 	while (++i < g->map->num_coins)
 	{
 		if (is_same_point(g->coins[i].pos, (t_point){-1, -1}))
 			continue ;
+
 		g->coins[i].current_frame = ++g->coins[i].current_frame % NUM_COIN_FRAMES;
 		render_sprite(g, &g->sp, g->coins[i].pos, S1);
 		render_sprite(g, &g->cframes, \
